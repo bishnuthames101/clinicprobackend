9444d54fd84874c8070c817c0522ffca35c152fc
12Bishnu <karkib523@gmail.com>
2025-06-06 20:54:26 +0545

    Updated  settings

--- patientrecords/wsgi.py
+++ patientrecords/wsgi.py
@@ -14,3 +14,14 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'patientrecords.settings')
 
 application = get_wsgi_application()
+
+from django.contrib.auth import get_user_model
+from django.core.management import call_command
+
+if os.environ.get("AUTO_CREATE_SUPERUSER", "False") == "True":
+    User = get_user_model()
+    if not User.objects.filter(username="admin").exists():
+        print("Creating default superuser...")
+        User.objects.create_superuser("admin", "admin@example.com", "adminpassword")
+    else:
+        print("Superuser already exists.")
